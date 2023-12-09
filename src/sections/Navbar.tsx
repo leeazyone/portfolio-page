69c165013fd85110467770df7c60d33684b65893
leeazyone <leejiwon317@naver.com>
2023-12-10 04:22:46 +0900

    4

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -68,31 +68,6 @@ function Navbar() {
             <Logo />
           </Link>
         </motion.div>
-        <motion.div
-          className='nav-responsive-toggle'
-          initial={{ opacity: 0, y: 5 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{
-            duration: 0.3,
-            ease: 'easeInOut',
-          }}
-        >
-          {responsiveNavVisible ? (
-            <CgClose
-              onClick={(e) => {
-                e.stopPropagation()
-                setResponsiveNavVisible(false)
-              }}
-            />
-          ) : (
-            <GiHamburgerMenu
-              onClick={(e) => {
-                e.stopPropagation()
-                setResponsiveNavVisible(true)
-              }}
-            />
-          )}
-        </motion.div>
         <div
           className={`${responsiveNavVisible && 'nav-responsive'} nav-items`}
         >
